c413633952dcbe2888952feab54d460e1804e07b
Rescuwu <189853556+Rescuwu@users.noreply.github.com>
2024-12-01 23:56:22 +0100

    feat: implement day 1 part 2
    Restructured how it reads the file into a function, for both parts to use.

--- day1/Main2.py
+++ day1/Main2.py
@@ -14,10 +14,24 @@ def timeit(func):
         return result
     return timeit_wrapper
 
-
-@timeit
-def Part1():
-    #vars
+def timeit100(func):
+    @wraps(func)
+    def timeit_wrapper(*args, **kwargs):
+        Nyatime=0
+        for x in range(100):
+            start_time = time.perf_counter()
+            result = func(*args, **kwargs)
+            end_time = time.perf_counter()
+            total_time = end_time - start_time
+            Nyatime+=total_time
+        Nyatime/=100
+        #print(f'Function {func.__name__}{args} {kwargs} Took {total_time:.4f} seconds')
+        print(f'Function {func.__name__}Took {Nyatime:.6f} seconds')
+        return result
+    return timeit_wrapper
+#reads file and puts into list and returns
+def filehandler():
+     #vars
     l1=[]
     l2=[]
     Distance=0
@@ -32,6 +46,13 @@ def Part1():
 
     #sorts lists
     l1.sort(),l2.sort()
+    return l1,l2
+
+@timeit
+def Part1():
+    #vars
+    Distance=0
+    l1,l2 = filehandler()
 
     # adding distance to total
     for x in range(len(l1)):
@@ -39,11 +60,24 @@ def Part1():
         Distance+=abs(l2[x]-l1[x])
     return Distance
 
+@timeit
 def Part2():
+    #vars
+    total=0
+    l1,l2 = filehandler()
 
-    return None
+    for x in l1:
+        k=0
+        for y in l2:
+            if x==y:
+                k+=y
+            elif x<y:
+                break
+        total+=k
+    return total
 
 if __name__ == "__main__":
     #answer printed
     print(Part1())
+    print(Part2())
     
